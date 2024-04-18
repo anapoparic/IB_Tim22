305f429db79a785393a270ed79534fd37403734c
anapoparic <anapoparic28@gmail.com>
2024-04-18 14:56:32 +0200

    CertificatesBackend setup

---
+++ certificatesBackend/src/main/java/com/example/certificatesbackend/CertificatesBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.certificatesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CertificatesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificatesBackendApplication.class, args);
+	}
+
+}
