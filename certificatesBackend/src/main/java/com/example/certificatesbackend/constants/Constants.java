04c77866993abc0f4bf8d5f5ce2970dd334fede5
anapoparic <anapoparic28@gmail.com>
2024-04-22 00:52:15 +0200

    Create root certificate done

--- certificatesBackend/src/main/java/com/example/certificatesbackend/constants/Constants.java
+++ certificatesBackend/src/main/java/com/example/certificatesbackend/constants/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 
     public static  String KEYSTORE_PASSWORD = "password";
     public static  String KEYSTORE_PATH = "src/main/resources/keystores/keystore.jks";
+    public static  String KEYSTORES_FOLDER_PATH = "src/main/resources/keystores";
     public static String ROOT_ALIAS = "ljubicica_lepaa";
 }
